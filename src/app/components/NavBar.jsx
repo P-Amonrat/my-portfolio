5e1138852757b37d822b40bbbc19ceed6a8cb43d
P.Amonrat <amonratpongka@gmail.com>
2025-05-27 20:05:21 +0700

    feat: banner home page

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const NavBar = () => {
   return (
     <nav className="bg-black p-4">
-      <div className="max-w-7xl mx-auto flex justify-between items-center text-white">
+      <div className="mx-auto flex justify-between items-center text-white">
         <div className="text-xl font-bold">MyPortfolio</div>
 
         {/* Desktop Menu */}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
-import Image from "next/image";
-import Link from "next/link";
-
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="bg-[url(/images/bg-com.jpg)] bg-cover bg-center h-screen bg-center">
+      <div className="flex flex-col justify-center h-full">
+        <div className="p-30">
+          <h1 className="text-5xl font-bold text-white ">AMONRAT</h1>
+          <h1 className="text-7xl font-bold text-white mt-1 ">PONGKA</h1>
+          <p className="text-2xl text-white mt-5 text-2sm">
+            FRONTEND DEVELOPER / FULL-STACK DEVELOPER
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
